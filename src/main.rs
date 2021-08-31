b817d58f14d984251a6ffd918f829017658b6bcd
Cameron <cjpuffnstuff@gmail.com>
2021-08-31 11:14:38 -0700

    Commit some nice code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+extern crate system_shutdown;
+
 use error::Error;
-use std::fs::{File};
-use std::io::Read;
-use std::path::Path;
-use std::{thread, time};
+use std::fs::File;
+use std::io::{stdin, BufRead, Read};
 use std::matches;
+use std::path::Path;
+use std::sync::mpsc::{self, Sender, TryRecvError};
+use std::{thread, time};
+use std::process::Command;
+use notify_rust::{Notification, Urgency};
+use time::Duration;
+use system_shutdown::shutdown;
 
 pub mod error;
 
@@ -12,35 +19,113 @@ pub enum PowerLevel {
     SHUTTING_DOWN,
     CRITICAL,
     LOW,
+    NORMAL,
     CHARGING,
     UNKNOWN,
 }
 
 fn main() {
-
     let mut state: PowerLevel = PowerLevel::UNKNOWN;
+    let (txx, rxx) = mpsc::channel(); // Default initialisation
+    let mut tx: Sender<()> = txx;
 
     loop {
         match read_battery_charge() {
             Ok(mut p) => {
-                p = 5;
-                // For testing purposes
+                match on_ac() {
+                    Ok(mut b) => {
+                        // For testing purposes
+                        //p = 2;
 
-                if p <= 5 && !matches!(state, PowerLevel::CRITICAL) {
-                    state = PowerLevel::CRITICAL;
-                    println!("Power state is now CRITICAL.");
+                        if p <= 5 && p > 3 && !matches!(state, PowerLevel::CRITICAL) && !b {
+                            state = PowerLevel::CRITICAL;
+                            println!("Power state is now CRITICAL.");
+                        }
+                        if p <= 20 && p > 5 && !matches!(state, PowerLevel::LOW) && !b {
+                            state = PowerLevel::LOW;
+                            println!("Power state is now LOW.");
+                        }
+                        
+                        if p > 20 && !matches!(state, PowerLevel::NORMAL) && !b {
+                            state = PowerLevel::NORMAL;
+                            println!("Power state is now NORMAL.");
+                        }
+
+                        if p <= 3 && !matches!(state, PowerLevel::SHUTTING_DOWN) && !b {
+                            state = PowerLevel::SHUTTING_DOWN;
+                            println!("Shutdown initiated!");
+                            tx = spawn_shutdown_task();
+                        }
+
+                        if b {
+                            if matches!(state, PowerLevel::SHUTTING_DOWN) {
+                                tx.send(());
+                            }
+                            if !matches!(state, PowerLevel::CHARGING) {
+                                state = PowerLevel::CHARGING;
+                                println!("Computer is now on AC power");
+                            }
+                        }
+                    }
+                    Err(_) => {
+                        println!("Failed to check if laptop is on battery");
+                    }
                 }
-                println!("Power {}", p);
-
-
-
             }
             Err(_) => {
-                println!("Failed");
+                println!("Failed to check battery charge level");
             }
         }
-        thread::sleep(time::Duration::from_millis(5000));
+        thread::sleep(Duration::from_millis(500));
     }
+}
+
+pub fn spawn_shutdown_task() -> Sender<()> {
+    let (tx, rx) = mpsc::channel();
+
+    let mut i = 60;
+
+    thread::spawn(move || loop {
+        println!("Shutdown...{}", i);
+        Notification::new().summary(&format!("Low power shutdown iminent in {} to prevent damage to battery", i)).appname("Battery").urgency(Urgency::Critical).timeout(1009).show();
+
+        if (i <= 0) {
+                match shutdown() {
+                    Ok(_) => println!("Shutting down, bye!"),
+                    Err(error) => eprintln!("Failed to shut down: {}", error),
+                }
+        }
+
+        i = i - 1;
+        thread::sleep(Duration::from_millis(1000));
+        match rx.try_recv() {
+            Ok(_) | Err(TryRecvError::Disconnected) => {
+                println!("Terminating shutdown thread.");
+                break;
+            }
+            Err(TryRecvError::Empty) => {}
+        }
+    });
+
+    let mut line = String::new();
+    let stdin = stdin();
+
+    tx
+}
+
+pub fn on_ac() -> Result<bool, Error> {
+    if !Path::new("/sys/class/power_supply/AC0/online").exists() {
+        println!("Unexpected, the directory /sys/class/power_supple/AC0/online doesn't exist? Do you not have a power source?");
+        return Ok(true);
+    }
+
+    let mut pwr_str: String = String::new();
+    File::open("/sys/class/power_supply/AC0/online")?.read_to_string(&mut pwr_str)?;
+
+    // Remove the \n char
+    pwr_str.pop();
+
+    return Ok(pwr_str == "1");
 }
 
 pub fn read_battery_charge() -> Result<i8, Error> {
